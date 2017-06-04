41357e53d2c7f57c24a931d25eac0762c1f4413d
tan-meng-hui <yashiro32@yahoo.com>
2017-06-04 17:14:29 +0800

    Spawning AI from C++.

---
+++ Source/S05_TestingGrounds/FP_FirstPerson/InfiniteTerrainGameMode.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FP_FirstPerson/FP_FirstPersonGameMode.h"
+#include "InfiniteTerrainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S05_TESTINGGROUNDS_API AInfiniteTerrainGameMode : public AFP_FirstPersonGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	AInfiniteTerrainGameMode();
+
+	UFUNCTION(BlueprintCallable, Category = "Bounds Pool")
+	void PopulateBoundsVolumePool();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pool")
+	class UActorPool* NavMeshBoundsVolumePool;
+
+private:
+	void AddToPool(class ANavMeshBoundsVolume *VolumeToAdd);
+	
+};
